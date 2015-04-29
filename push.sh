230704483fc6ad94553e8ed73cb4de20dbb5ea81
Steevan BARBOYON <s.barboyon@c2is.fr>
2015-04-29 17:48:30 +0200

    Add push.sh

---
+++ push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source "$(dirname $0)/functions.sh"
+
+gitBranch="$(getGitBranch)"
+if [ "$gitBranch" == "" ]; then
+    cancelScript "Branch can't be found. Maybe you are not in a dir associated to a git repository ?"
+fi
+
+title "Push branch $gitBranch to origin"
+execCmd "git push origin $gitBranch"
+
+echoOk
