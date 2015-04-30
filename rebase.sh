c8dc9eb651517b64af740b08f943999ad02b9e12
Steevan BARBOYON <s.barboyon@c2is.fr>
2015-04-30 17:13:22 +0200

    Same style for all scripts

--- rebase.sh
+++ rebase.sh
@@ -28,22 +28,18 @@ countEditedFiles="${#editedFiles[@]}"
 if [ "$stash" == "ask" ] && [ "$countEditedFiles" -gt "0" ]; then
     ask "Repository contains unstaged changes. Do you want to stash them ? (Y/n)"
     read readStash
-    echo ''
     if [ "$readStash" == "" ] || [ "$readStash" == "y" ] || [ "$readStash" == "Y" ]; then
         stash="yes"
     fi
 fi
 if [ "$countEditedFiles" -gt "0" ] && [ "$stash" == "yes" ]; then
     execCmd "git stash"
-    echo ""
 fi
 
 execCmd "git fetch"
-echo ""
 execCmd "git rebase -p $rebaseFrom"
 
 if [ "$countEditedFiles" -gt "0" ] && [ "$stash" == "yes" ]; then
-    echo ""
     execCmd "git stash pop"
 fi
 
